be19835bdbe628a03f07870bc5c678acdef03ad8
Jay Deaton <jayrdeaton@gmail.com>
2026-05-17 15:45:08 +0700

    bundler config fix

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,12 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
   moduleNameMapper: {
     '^react-native-svg$': '<rootDir>/src/__mocks__/react-native-svg.ts',
     '^react-native$': '<rootDir>/src/__mocks__/react-native.ts'
   },
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', { tsconfig: { jsx: 'react' } }]
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: { jsx: 'react', module: 'CommonJS', moduleResolution: 'node', ignoreDeprecations: '5.0' } }]
   },
   testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
