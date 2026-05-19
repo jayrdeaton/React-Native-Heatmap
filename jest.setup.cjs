7374f7123009593a0a93cab1e0234e9878d43354
Jay Deaton <jayrdeaton@gmail.com>
2026-05-19 08:27:15 +0700

    ci fix

--- jest.setup.cjs
+++ jest.setup.cjs
@@ -1,4 +1,4 @@
-// Suppress unhandled promise rejections from test noise
+/* eslint-disable no-console */
 process.on('unhandledRejection', (reason) => {
   console.error('Unhandled rejection in test:', reason)
 })
